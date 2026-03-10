15e7016101dfb8ff0e186effe2e690ecc3457769
Arandelle <topaguintsarandell@gmail.com>
2026-03-10 15:53:28 +0800

    feat: improve the design

--- src/pages/MissionVisionPage.tsx
+++ src/pages/MissionVisionPage.tsx
@@ -1,8 +1,7 @@
-import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
 import { MedalIcon, MapIcon } from "../components/Icons";
 import target from "../assets/target.png";
 import vis from "../assets/vision.png";
-import misvislogo from "../assets/vision-and-mission.png";
+
 interface FeatureProps {
   icon: JSX.Element;
   title: string;
@@ -31,53 +30,35 @@ export const MissionVisionPage = () => {
   return (
     <section
       id="missionandvision"
-      className="container text-center py-24 sm:py-32"
+      className="relative container text-center py-12"
       data-aos="fade-up"
     >
-      <h2 className="text-3xl lg:text-4xl font-bold md:text-center flex items-center justify-center gap-4">
-        Our{" "}
-        <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-          Mission and Vision{" "}
-        </span>
-        <img
-          src={misvislogo}
-          alt="analytics"
-          className="w-[55px] h-[55px]" // Adjusted size for alignment
-        />
+      <h2 className=" text-4xl lg:text-4xl font-bold md:text-center flex items-center justify-center gap-4 py-8">
+        Our <span className="text-primary uppercase">Mission </span>
+        <span>and</span>
+        <span className="text-primary uppercase">Vision </span>
       </h2>
-      <p className="md:w-3/4 mx-auto mt-4 mb-8 text-xl text-muted-foreground ">
-        
-      </p>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
         {features.map(({ image, title, description }: FeatureProps) => (
-          <Card
-         
+          <div
             key={title}
-            className="bg-muted/50  transform hover:scale-105 hover:shadow-lg"
+            className={`bg-muted/50  transform ${title === "Mission" ? " hover:rotate-1" : "hover:-rotate-1"}  hover:shadow-lg`}
           >
-            <CardHeader>
-              <CardTitle className="grid gap-4 place-items-center ">
-                
-                {title}
-              </CardTitle>
-            </CardHeader>
+            <h1 className="font-bold py-4 text-xl">{title}</h1>
             <img
-                src={image} // Use the `image` property from the `features` array
-                alt={`${title} illustration`}
-                className="mt-1 w-1/6 h-auto mx-auto rounded-md" // Adjusted width to 3/4 and centered
-              />
-     
-           
-            <CardContent>
-              <p className="text-lg md:text-xl text-muted-foreground mt-5 justify-center">{description}</p> {/* Increased font size */}
-            </CardContent>
-          </Card>
+              src={image} // Use the `image` property from the `features` array
+              alt={`${title} illustration`}
+              className="mt-1 w-1/6 h-auto mx-auto rounded-md" // Adjusted width to 3/4 and centered
+            />
+            <div className="py-8 px-16">
+              <p className="text-md md:text-lg text-justify py-8">
+                {description}
+              </p>{" "}
+            </div>
+          </div>
         ))}
       </div>
-
-
-      
     </section>
   );
 };
