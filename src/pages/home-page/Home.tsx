f69fafeed35a8a77ba13ca1dfd1f38ca9911b1b3
Arandelle <topaguintsarandell@gmail.com>
2026-04-30 18:49:04 +0800

    feat: remve unused import

--- src/pages/home-page/Home.tsx
+++ src/pages/home-page/Home.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Hero } from "./components/Hero";
 import GroupOverview from "./components/GroupOverview";
 import { Subsidaries } from "./components/Subsidaries";
